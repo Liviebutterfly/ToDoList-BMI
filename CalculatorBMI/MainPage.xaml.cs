3e4fcaa96b6dad9f535cdcfe1c0c665663c16b1e
Reve <Reve@DESKTOP-EK7RNHL>
2026-02-25 21:49:30 +0800

    Add project files.

---
+++ CalculatorBMI/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculatorBMI;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void OnCalculateButtonClicked(object sender, EventArgs e)
+    {
+        string weightText = WeightEntry.Text;
+        string heightText = HeightEntry.Text;
+
+        if (double.TryParse(weightText, out double weight) && double.TryParse(heightText, out double height) && height > 0)
+        {
+            double bmi = weight / (height * height);
+            ResultLabel.Text = $"Your BMI: {bmi:F2}";
+        }
+        else
+        {
+            ResultLabel.Text = "Please enter valid numbers for weight and height.";
+        }
+    }
+}
